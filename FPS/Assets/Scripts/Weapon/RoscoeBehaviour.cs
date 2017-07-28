5a090946131b2870ae6bbd2498c01115e859db0a
kompyang <linyuwei1995@gmail.com>
2017-07-29 02:10:08 +0800

    大概增加了子弹行为

---
+++ FPS/Assets/Scripts/Weapon/RoscoeBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    Value<int> bulletCapacity;
+    public float MaxCapacity;
+    bool isNull;
+
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
