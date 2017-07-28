ec3fc3ac9d7af297daa4bb20e086b29fcea3ba03
玉伟 林 <linyuwei1995@gmail.com>
2017-07-28 19:18:52 +0800

    hud
    增加了血量的hud

---
+++ FPS/Assets/Scripts/HUD/HealthHud.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHud : MonoBehaviour
+{
+
+    public Slider HealthSolider;
+    public Text HealthTest;
+    Value<float> L_PlayerHealth;
+
+    private void Awake()
+    {
+        L_PlayerHealth = GameplayStatics.LocalPlayer.health;
+        L_PlayerHealth.AddChangedListener(ChangeHealth);
+        HealthTest.text = "Health: " + L_PlayerHealth.Get().ToString();
+       
+    }
+
+    void ChangeHealth()
+    {
+        if (L_PlayerHealth.Get() == L_PlayerHealth.GetPreviousValue())
+        {
+            return;
+        }
+        HealthTest.text = "Health: " + GameplayStatics.LocalPlayer.health.Get().ToString();
+        HealthSolider.value = L_PlayerHealth.Get()*0.01f;
+    }
+
+
+
+
+}
---
+++ FPS/Assets/Scripts/Trigers/MeatSphereTriger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatSphereTriger : MonoBehaviour
+{
+
+    public float damageValue;
+    Value<float> L_Phealth;
+   
+
+    private void Awake()
+    {
+        L_Phealth = GameplayStatics.LocalPlayer.health;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("执行了伤害方法 ");
+            GameplayStatics.LocalPlayer.health.Set(GetDamedHealth(damageValue));
+        }
+    }
+
+
+
+
+
+    #region unsortedCode
+
+
+    /// <summary>
+    /// 返回受到伤害后的生命值
+    /// </summary>
+    /// <param name="d_value">输入伤害的数值</param>
+    /// <returns></returns>
+    float GetDamedHealth(float d_value)
+    {
+        Debug.Log("受到了伤害");
+        float c_health = L_Phealth.Get() - d_value;
+        return c_health;
+    }
+    #endregion
+
+}
