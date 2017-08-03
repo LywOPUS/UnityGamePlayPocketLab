161080bbbb0577583dab13390e9372f82d568554
kompyang <linyuwei1995@gmail.com>
2017-08-04 02:57:47 +0800

    做了些小改动

--- VectorProject/MousePointMove/Assets/GetMousePointMove.cs
+++ VectorProject/MousePointMove/Assets/GetMousePointMove.cs
@@ -1,44 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GetMousePointMove : MonoBehaviour {
-
-    private Vector3 target;
-    private bool isOver = true;
-    public float speed;
+public class GetMousePointMove : MonoBehaviour
+{
     public Transform GO;
-	
-	void Update () {
+    public float speed;
+    private bool isOver = true;
+    private Vector3 target;
+
+    /// <summary>
+    /// 移动物体到目标点位置
+    /// </summary>
+    /// <param name="_target">目标点</param>
+    /// <param name="GO">要移动的物体</param>
+    private void MoveToMouse(Vector3 _target, Transform GO)
+    {
+        if (!isOver)
+        {
+            Vector3 offset = _target - GO.transform.position;
+            GO.position += offset.normalized * speed * Time.deltaTime;
+            if (Vector3.Distance(_target, transform.position) < 0.5F)
+            {
+                isOver = true;
+                transform.position = _target;
+            }
+        }
+    }
+
+    private void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hitInfo = new RaycastHit();
-            if (Physics.Raycast(ray,out hitInfo))
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                if (hitInfo.collider.name =="Ground")
+                if (hitInfo.collider.name == "Plane")
                 {
+                    Debug.Log("isHit");
                     target = hitInfo.point;
                     target.y += 0.5f;
                     isOver = false;
                 }
             }
-           
         }
 
-	}
-    private void MoveToMouse(Vector3 _target,Transform GO)
-    {
-        if (!isOver)
-        {
-            Vector3 offset = target - GO.transform.position;
-            GO.position += offset.normalized * speed * Time.deltaTime;
-            if (Vector3.Distance(target,transform.position)<0.5F)
-            {
-                isOver = true;
-                transform.position = target;
-            }
-        }
+        MoveToMouse(target, GO);
     }
 }
