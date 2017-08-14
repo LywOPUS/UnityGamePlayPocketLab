7045837cb651816f9812515bc20f198df0946681
玉伟 林 <linyuwei1995@gmail.com>
2017-08-14 20:07:09 +0800

    上传ui代码

---
+++ UIManager/Assets/Scenes/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public GameObject Com_Item;
+
+    public RectTransform rectTran;
+
+    public Button Btn_AddItem;
+
+    public GridLayoutGroup grid;
+
+    public int currentItemNum;
+
+    void Start()
+    {
+        Btn_AddItem.onClick.AddListener(AddItem);
+        Com_Item = Resources.Load("assetsbundles/ui/Com_Item") as GameObject;
+    }
+
+    public void AddItem()
+    {
+        var obj = GameObject.Instantiate(Com_Item) as GameObject;
+        obj.transform.parent = rectTran;
+        obj.gameObject.transform.localScale = Vector3.one;
+        currentItemNum++;
+
+        ChangeHight();
+    }
+
+    public void ChangeHight()
+    {
+        float itemHight = grid.spacing.y + grid.cellSize.y;
+
+        rectTran.sizeDelta = new Vector2(0, itemHight * Mathf.Ceil((float)currentItemNum / grid.constraintCount));
+    }
+}
---
+++ UIManager/Assets/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// 在开始界面加载所有的文件到场景中？
+    /// 用事件管理所有的ui事件。
+    ///
+    ///
+    /// </summary>
+    private Button AddWepon;
+
+    private RectTransform UICanvas;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
+
+//资源加载
+public class Item
+{
+    private GameObject Wepon;
+    private GameObject Armor;
+    private GameObject sundry;
+    //构造函数
+}
