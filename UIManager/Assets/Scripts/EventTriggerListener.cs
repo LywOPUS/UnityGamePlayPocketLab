d0f57d351f772247c9ad5b578178a163cdc95e6e
玉伟 林 <linyuwei1995@gmail.com>
2017-08-17 20:14:40 +0800

    提交源代码

---
+++ UIManager/Assets/Scripts/EventTriggerListener.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using System;
+
+public class EventTrigerLisner : MonoBehaviour, IPointerUpHandler, IDragHandler, IBeginDragHandler, IPointerClickHandler
+{
+    public UnityAction onClick;
+    public UnityAction<PointerEventData> onDrag;
+    public UnityAction<PointerEventData> onBeginDrag;
+    public UnityAction<PointerEventData> onPointerUp;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (onBeginDrag != null)
+        {
+            onBeginDrag(eventData);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (onClick != null)
+        {
+            onClick();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (onPointerUp != null)
+        {
+            onPointerUp(eventData);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (onDrag != null)
+        {
+            onDrag(eventData);
+        }
+    }
+}
---
+++ UIManager/Assets/Scripts/UIBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+    public EventTrigerLisner SetEventTrigger(GameObject rObj)
+    {
+        var eventListener = rObj.GetComponent<EventTrigerLisner>();
+        if (eventListener == null)
+        {
+            eventListener = rObj.AddComponent<EventTrigerLisner>();
+        }
+
+        return eventListener;
+    }
+}
